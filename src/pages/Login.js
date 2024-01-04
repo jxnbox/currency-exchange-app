2b62c0d657bb5d14bba7844920c6694faead5bee
jxnbox <janr3p@gmail.com>
2024-01-04 20:18:39 +0000

    adding form in login page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,9 +2,27 @@ import { Component } from "react";
 
 class LoginPage extends Component {
 
+    handleFormSubmit(e) {
+        e.preventDefault();
+    }
+
+
     render() {
         return (
-            <h2>Login page</h2>
+            <div>
+                <h2>Login page</h2>
+                <div>
+                    <form onSubmit={this.handleFormSubmit}>
+                        <label htmlFor="username">username: </label>
+                        <input type="text" name="username" id="username" />
+                        <br />
+                        <label htmlFor="password">password: </label>
+                        <input type="password" name="password" id="password" />
+                        <br />
+                        <button>continue</button>
+                    </form>
+                </div>
+            </div>
         )
     }
 }
