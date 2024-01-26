cc0a74b983e51ea4fb5230e8bfb229d2d824e6e2
jxnbox <janr3p@gmail.com>
2024-01-26 15:41:53 +0000

    editing login page using bootstrap

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -3,6 +3,12 @@ import { signInWithEmailAndPassword } from "firebase/auth";
 import CurrencyExchangePage from "./currencyExchangePage";
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Stack from 'react-bootstrap/Stack';
+
+
 const LoginPage = ({updateUserState, auth}) => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
@@ -44,18 +50,20 @@ const LoginPage = ({updateUserState, auth}) => {
 
     if (!isLoggedIn) {
         return (
-            <div>
-                <h2>Login page</h2>
-                <div>
-                    <Form onSubmit={handleFormSubmit}>
-                        <Form.Label htmlFor="email">email: </Form.Label>
-                        <Form.Control type="text" name="email" onChange={handleOnChangeEmail}/>
-                        <Form.Label htmlFor="password">password: </Form.Label>
-                        <Form.Control type="password" name="password" onChange={handleOnChangePassword}/>
-                        <Button variant="primary" type="submit">continue</Button>
-                    </Form>
-                </div>
-            </div>
+            <Container>
+                <Stack gap={3}>
+                        <h2>Login page</h2>
+                        <Form onSubmit={handleFormSubmit}>
+                            <Stack gap={2}>
+                                <Form.Label htmlFor="email">email: </Form.Label>
+                                <Form.Control type="text" name="email" onChange={handleOnChangeEmail}/>
+                                <Form.Label htmlFor="password">password: </Form.Label>
+                                <Form.Control type="password" name="password" onChange={handleOnChangePassword}/>
+                            </Stack>
+                            <Button variant="primary" className="login-submit-btn" type="submit">continue</Button>
+                        </Form>
+                </Stack>
+            </Container>
         );
     } else {
         return <CurrencyExchangePage />
