e657cb866dbd00170fc33e3c00a59c5d120d574c
jxnbox <janr3p@gmail.com>
2024-01-31 20:23:05 +0000

    adding bootstrap to results

--- src/components/DisplayResult.jsx
+++ src/components/DisplayResult.jsx
@@ -1,9 +1,20 @@
 import { Component } from "react";
+import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup';
 
 class DisplayResult extends Component {
 
     render() {
-        return <p>Result: {this.props.result}</p>
+        return (
+            <InputGroup className="mb-3">
+                <InputGroup.Text id="basic-addon1">Result:</InputGroup.Text>
+                <Form.Control 
+                type="text"
+                placeholder={`${this.props.result}`}
+                aria-label="Disabled input example"
+                readOnly/>
+            </InputGroup>
+        )
     }
 }
 
